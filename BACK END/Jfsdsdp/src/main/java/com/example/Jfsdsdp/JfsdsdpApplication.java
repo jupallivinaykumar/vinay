2fc59a0dd8a25d1db8a7bfdc26ddea1dc364fc85
jupallivinaykumar <142559492+jupallivinaykumar@users.noreply.github.com>
2024-12-10 12:01:39 +0530

    Add files via upload

---
+++ BACK END/Jfsdsdp/src/main/java/com/example/Jfsdsdp/JfsdsdpApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jfsdsdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsdsdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfsdsdpApplication.class, args);
+	}
+
+}
---
+++ BACK END/Jfsdsdp/src/test/java/com/example/Jfsdsdp/JfsdsdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jfsdsdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfsdsdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
